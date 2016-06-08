92b2de2de0498d669c8449534296b497b5378a36
Guest User <aleah.luk@gmail.com>
2016-06-08 12:36:24 +0800

    finally finished binary search... omg it was death

--- binary-search.rb
+++ binary-search.rb
@@ -1,1 +1,26 @@
 # Place your solutions here
+
+def binary_search(value, array, low = 0, high = array.length - 1, x = 0)
+    while array[x] != value
+        if low - high == (1 || -1)
+            return -1
+            break
+        end
+        x = ((high+low)/2).floor
+        if array[x] < value
+            low = x + 1
+            elsif array[x] > value
+            high = x - 1
+        end
+    end
+    return x
+end
+
+
+test_array = (100..200).to_a
+puts binary_search(135, test_array) == 35
+# => true
+
+test_array = [13, 19, 24, 29, 32, 37, 43]
+puts binary_search(35, test_array) == -1
+# => true
